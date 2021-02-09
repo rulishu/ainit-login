c499c8bf82a346598c9ecab0b06e1cf6366f7fd1
xiaoshu <1324214367@qq.com>
2021-02-09 16:35:08 +0800

    提交参数到后台，并获取返回

--- src/App.js
+++ src/App.js
@@ -22,12 +22,46 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      
+      user: '',
+      pass: '',
+      list:[],
     };
   }
  
   componentDidMount(){
     axios.get('http://localhost:3000/login').then(res=>{console.log(res)});
+  }
+
+  handleChange(event, type){
+    this.setState({[type]: event.target.value});
+  }
+
+  sub(){
+    let data = {list:this.state};
+    console.log(data);
+    console.log(this.state.user+":"+this.state.pass);
+    axios.post('http://localhost:3000/user', {
+      user: this.state.user,
+      pass: this.state.pass
+    })
+    .then(res=>{
+      console.log(res);
+      console.log(res.data);
+      if (res.data[0].num!=1) {
+        console.log("重新输入...");
+        alert("用户名密码错误，请重新输入。");
+      }else{
+        console.log("跳转....");
+        window.open("http://localhost:3000/login");
+      }
+      this.setState({user:res.data});
+    })
+    .catch(res=>{console.log('err')});
+  };
+  
+  exit(){
+    
+    
   }
 
   render() {
@@ -37,20 +71,20 @@ class App extends React.Component {
          <ul className='grandson'>
             <li className='login'>账号登录</li>
             <li className='username'>
-              <input type="text" placeholder='用户名'/>
+              <input type="text" placeholder='用户名' name='user' onChange={(event) => this.handleChange(event, 'user')}/>
             </li>
             <li className='password'>
-              <input type="password" placeholder='密码'/>
+              <input type="password" placeholder='密码' name='pass' onChange={(event) => this.handleChange(event, 'pass')}/>
             </li>
-           <button className='sub' type="submit">submit</button>
-           <button className='exit' >exit</button>
+           <button className='sub' onClick={this.sub.bind(this)}>submit</button>
+           <button className='exit' onClick={this.exit.bind(this)}>exit</button>
          </ul>
       </div>
     </div>
-    
     );
   }
 }
 
-
 export default App;
+
+
--- src/mysql.js
+++ src/mysql.js
@@ -6,10 +6,10 @@
 var mysql  = require('mysql');  
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser');
+let bodyParser = require('body-parser');
 
 app.use(bodyParser.json())                          // json化
-app.use(bodyParser.urlencoded({ extended: false })) // 创建 application/x-www-form-urlencoded 编码解析
+app.use(bodyParser.urlencoded({ extended: false })) // 编码解析
  
 
 //第1步：连接数据库 确保连接运行。node src/mysql.js
@@ -28,11 +28,11 @@ connection.query('SELECT a_id,username,password FROM a1;', function (error, resu
 
 //第2步：先跨域，用到了express(http的封装包)
 app.all('*', function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-//   res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
   });
 
 //第3步：node.js暴露服务,
@@ -43,17 +43,13 @@ app.get('/login',function(req,res){
     })
 })
 app.post('/user',function(req,res){
-    // let user = req.body.user;
-    let sql = 'SELECT username FROM a1;';
-    connection.query(sql,function(request,response){
-        res.json(response)
-    })
-})
-app.post('/pass',function(req,res){
-    // let pass = req.body.pass;
-    let sql = 'SELECT password FROM a1;';
-    connection.query(sql,function(request,response){
-        res.json(response)
+    console.log(req);
+    let user = req.body.user;
+    let pass = req.body.pass;
+    let sql = 'select count(1) as num from a1 where username = \"'+user+'\" and password = \"'+pass+'\"';
+    connection.query(sql,function(error,result,filed){
+        console.log("数据库查询结果:" + result);
+        res.json(result);
     })
 })
 
