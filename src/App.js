5f8ebabca2c3b3dd310ab2a62857ee0b40475082
xiaoshu <1324214367@qq.com>
2021-02-10 17:32:44 +0800

    返回参数展示‘

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import axios from 'axios';
 
+
 // function App(props) {
 //   return (
 //     <div class='parent'>
@@ -24,61 +25,95 @@ class App extends React.Component {
     this.state = {
       user: '',
       pass: '',
-      list:[],
+      list:[
+      ],
     };
   }
  
   componentDidMount(){
-    axios.get('http://localhost:3000/login').then(res=>{console.log(res)});
-  }
+  //   axios.get('http://localhost:3200/login').then(res=>{
+  //   this.state.list = res.data ;
+  //   console.log(this.state.list);
+  // });
+  }//接收监听的端口，打印查看是否监听成功,拿到参数
 
   handleChange(event, type){
     this.setState({[type]: event.target.value});
-  }
+  }//获取页面参数，type类型为 user 和 pass
 
   sub(){
     let data = {list:this.state};
-    console.log(data);
     console.log(this.state.user+":"+this.state.pass);
-    axios.post('http://localhost:3000/user', {
+    //提交页面参数到后台
+    axios.post('http://localhost:3200/user', {  //post返回参数写法
       user: this.state.user,
       pass: this.state.pass
     })
     .then(res=>{
-      console.log(res);
       console.log(res.data);
+      //后台返回参数校验
       if (res.data[0].num!=1) {
         console.log("重新输入...");
         alert("用户名密码错误，请重新输入。");
       }else{
+        // debugger;
         console.log("跳转....");
-        window.open("http://localhost:3000/login");
+        axios.get('http://localhost:3200/login', {params:{  //get返回参数写法
+        user: this.state.user,
+        pass: this.state.pass
+      }})
+      .then(res=>{
+          this.state.list = res.data ;
+          console.log(this.state.list);
+          this.forceUpdate();
+        });
+        // this.forceUpdate();
       }
-      this.setState({user:res.data});
+      // this.setState({user:res.data});
     })
     .catch(res=>{console.log('err')});
   };
   
   exit(){
-    
-    
+
   }
 
   render() {
     return (
     <div className='parent'>
       <div className='son'>
-         <ul className='grandson'>
-            <li className='login'>账号登录</li>
-            <li className='username'>
-              <input type="text" placeholder='用户名' name='user' onChange={(event) => this.handleChange(event, 'user')}/>
-            </li>
-            <li className='password'>
-              <input type="password" placeholder='密码' name='pass' onChange={(event) => this.handleChange(event, 'pass')}/>
-            </li>
-           <button className='sub' onClick={this.sub.bind(this)}>submit</button>
-           <button className='exit' onClick={this.exit.bind(this)}>exit</button>
-         </ul>
+        <ul className='grandson'>
+          <li className='login'>账号登录</li>
+          <li className='username'>
+            <input type="text" placeholder='用户名' name='user' onChange={(event) => this.handleChange(event, 'user')}/>
+          </li>
+          <li className='password'>
+            <input type="password" placeholder='密码' name='pass' onChange={(event) => this.handleChange(event, 'pass')}/>
+          </li>
+          <button className='sub' onClick={this.sub.bind(this)}>
+            submit
+          </button>
+          <button className='exit' onClick={this.exit.bind(this)}>exit</button>
+         
+        <table>
+          <thead>
+            <tr>
+              <th>a_id</th>
+              <th>username</th>
+              <th>password</th>
+            </tr>
+          </thead>
+          <tbody>
+           {this.state.list.map((item,index) =>
+            <tr key={index}>
+              <td>{item.a_id}</td>
+              <td>{item.username}</td>
+              <td>{item.password}</td>
+            </tr>
+           )}
+          </tbody>
+          </table>
+        </ul>
       </div>
     </div>
     );
--- src/mysql.js
+++ src/mysql.js
@@ -1,6 +1,6 @@
 //react+node.js+mysql
 //mysql inner.join left/right.join 
-//node.js->express->socket.io
+//node.js->express->socket.io npm-pm2全局状态管理
 //react class/function组件 hook钩子 生命周期
 
 var mysql  = require('mysql');  
@@ -12,7 +12,7 @@ app.use(bodyParser.json())                          // json化
 app.use(bodyParser.urlencoded({ extended: false })) // 编码解析
  
 
-//第1步：连接数据库 确保连接运行。node src/mysql.js
+//第1步：连接数据库
 let connection = mysql.createConnection({     
   host     : 'localhost',       
   user     : 'root',              
@@ -26,7 +26,7 @@ connection.query('SELECT a_id,username,password FROM a1;', function (error, resu
   console.log('连接成功');
 });
 
-//第2步：先跨域，用到了express(http的封装包)
+//第2步：跨域
 app.all('*', function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
@@ -35,14 +35,18 @@ app.all('*', function (req, res, next) {
     next();
   });
 
-//第3步：node.js暴露服务,
-app.get('/login',function(req,res){
-    let sql = 'SELECT * from a1';
+//第3步：暴露服务,
+app.get('/login',function(req,res){  //get请求用query传参
+    let user = req.query.user; 
+    let pass = req.query.pass;
+    console.log("get 请求体："+req.body);
+    console.log("get 请求参数："+user+":"+pass);
+    let sql = 'SELECT * from a1 where username = \"'+user+'\" and password = \"'+pass+'\"';
     connection.query(sql,function(request,response){
         res.json(response)
     })
 })
-app.post('/user',function(req,res){
+app.post('/user',function(req,res){  //post请求用body带参
     console.log(req);
     let user = req.body.user;
     let pass = req.body.pass;
@@ -53,9 +57,9 @@ app.post('/user',function(req,res){
     })
 })
 
-//监听3000端口
-app.listen(3000, function () {
-  console.log('成功监听 3000 端口')
+//监听3200端口
+app.listen(3200, function () {
+  console.log('成功监听 3200 端口')
 });
 
 
